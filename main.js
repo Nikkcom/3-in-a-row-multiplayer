717d718de2e643440b1f03fd697472b23a04ae40
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2025-01-27 19:26:22 +0000

    Fix incorrect sharable link

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function receiveMoves(playingBoard, websocket) {
                 // Creates the shareable link to join the game
                 const shareInput = document.getElementById("share-link");
                 const joinKey = event.join_key
-                shareInput.value = window.location.origin + "?join_key=" + joinKey;
+                shareInput.value = window.location.origin + "/3-in-a-row-multiplayer/?join_key=" + joinKey;
                 break;
             case "PLAY":
                 // In case of PLAY event. Update the UI with the opponents move.
