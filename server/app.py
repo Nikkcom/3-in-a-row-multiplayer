67fa0317cd00a11bcf826ede163320ba736ae137
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2025-01-27 16:45:34 +0000

    Refactoring

--- server/app.py
+++ server/app.py
@@ -1,11 +1,9 @@
 import asyncio
 import json
 import itertools
-from multiprocessing.managers import Array
+
 from secrets import token_urlsafe
-
 from websockets.asyncio.server import broadcast, serve
-
 from threeinarow import PLAYER_ONE, PLAYER_TWO, Threeinarow
 
 JOIN = {}
@@ -14,7 +12,6 @@ async def error(websocket, message):
     """
     Send an error message
     """
-
     event = {
         "type": "ERROR",
         "message": message
@@ -52,30 +49,23 @@ async def play(websocket, game, player, connected):
     """
     Receive and process moves from a player
     """
-
     async for message in websocket:
         # Logging
         print(f"Received message in play: {message}")
 
         # Parse a PLAY event from the UI
         event = json.loads(message)
-
         assert(event["type"] == "PLAY")
-
         row = event["row"]
         column = event["column"]
 
         try:
-
             # Plays the move
             game.play(PLAYER_ONE, column, row)
-
         except ValueError as exc:
-
             # Send an ERROR event if the move was illegal.
             await error(websocket, str(exc))
             continue
-
         event = {
             "type": "PLAY",
             "player": player,
@@ -97,38 +87,28 @@ async def join(websocket, join_key):
     """
     Handles the connection for the second player. Joining an already initialized gme.
     """
-
-
     try:
         game, connected = JOIN[join_key]
-
     except KeyError:
         await error(websocket, "Game not found.")
         return
-
     # Adds the connection
     connected.add(websocket)
-
     try:
-
         await play(websocket, game, PLAYER_TWO, connected)
     finally:
         connected.remove(websocket)
-
 async def handler(websocket):
     """
     Handles a connection based on who is connecting
     """
-
     # Initializes a game.
     game = Threeinarow()
     print(f"Game initialized")
     print(f"Game= {len(game.board)}")
-
     # Players alternate turns in the same browser.
     turns = itertools.cycle([PLAYER_ONE, PLAYER_TWO])
     player = next(turns)
-
     async for message in websocket:
         print(f"Received message in handler: {message}")
         # Parses a PLAY event from the UI
@@ -136,9 +116,7 @@ async def handler(websocket):
         assert(event["type"] == "PLAY")
         row = event["row"]
         column = event["column"]
-
         try:
-
             # Tries to play the move.
             game.play(player, row, column)
             print(f"game.play success")
@@ -161,7 +139,6 @@ async def handler(websocket):
         }
         print(f"Sending event: {json.dumps(event)}")
         await websocket.send(json.dumps(event))
-
         if game.winner is not None:
             event = {
                 "type": "WIN",
@@ -170,13 +147,11 @@ async def handler(websocket):
             }
             print(f"Sending event: {json.dumps(event)}")
             await websocket.send(json.dumps(event))
-
         player = next(turns)
 
 async def main():
 
     async with serve(handler, "", 8001):
-
         await asyncio.get_running_loop().create_future()  # run forever
 
 if __name__ == "__main__":
